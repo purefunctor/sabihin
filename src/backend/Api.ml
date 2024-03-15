a2dabf1e812778e69da0dbe644ebf6b4bbe7f56d
Justin Garcia <purefunctor@gmail.com>
2024-03-15 08:53:53 +0800

    Use yojson for backend code

---
+++ src/backend/Api.ml
@@ -0,0 +1,1 @@
+let handler _ = Dream.respond ~status:`Bad_Request "<a>haha?\n\n</a>"
--- src/backend/Backend.ml
+++ src/backend/Backend.ml
@@ -1,3 +1,16 @@
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+module ErrorResponse = struct
+  type 'a t = { code : int; reason : string; content : 'a } [@@deriving yojson]
+
+  let make ~code ~reason ~content = { code; reason; content }
+
+  let make_string ~code ~reason ~content yojson_of_content =
+    make ~code ~reason ~content
+    |> yojson_of_t yojson_of_content
+    |> Yojson.Safe.to_string
+end
+
 let error_template _ _ suggested_response =
   let status = Dream.status suggested_response in
   let code = Dream.status_to_int status in
@@ -7,11 +20,26 @@ let error_template _ _ suggested_response =
       Dream.set_header suggested_response "Content-Type" Dream.text_html;
       Dream.set_body suggested_response @@ Pages.WithApp.render [ "not-found" ];
       Lwt.return suggested_response
-  | _ ->
-      Dream.set_header suggested_response "Content-Type" Dream.application_json;
-      Dream.set_body suggested_response
-        (Printf.sprintf {|"code": %i, "reason": "%s"|} code reason);
-      Lwt.return suggested_response
+  | _ -> (
+      match Dream.header suggested_response "Content-Type" with
+      | Some "application/json" ->
+          let%lwt content =
+            Dream.body suggested_response |> Lwt.map Yojson.Safe.from_string
+          in
+          let body = ErrorResponse.make_string ~code ~reason ~content Fun.id in
+          Dream.set_body suggested_response body;
+          Lwt.return suggested_response
+      | Some _ -> Lwt.return suggested_response
+      | None ->
+          let%lwt content =
+            Dream.body suggested_response
+            |> Lwt.map (fun content -> `String content)
+          in
+          let body = ErrorResponse.make_string ~code ~reason ~content Fun.id in
+          Dream.set_header suggested_response "Content-Type"
+            Dream.application_json;
+          Dream.set_body suggested_response body;
+          Lwt.return suggested_response)
 
 let error_handler = Dream.error_template error_template
 let interface = "0.0.0.0"
@@ -30,4 +58,5 @@ let () =
              Dream.get "/register" (fun _ ->
                  [ "register" ] |> Pages.WithApp.render |> Dream.html);
            ];
+         Dream.scope "/api" [] [ Dream.get "/register" Api.handler ];
        ]
