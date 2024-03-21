0199d870f1ea52322cb82705469d081abedeb50a
Justin Garcia <purefunctor@gmail.com>
2024-03-21 20:54:08 +0800

    Initialize NanoID before anything else

--- lib/models/Initialize.ml
+++ lib/models/Initialize.ml
@@ -2,8 +2,8 @@ open Extra
 
 let initialize db =
   let ( let* ) = Lwt_result.bind in
+  let* _ = Nanoid.initialize db in
   let* _ = User.create_table db in
   let* _ = PrivateKeys.create_table db in
   let* _ = PublicKeys.create_table db in
-  let* _ = Nanoid.initialize db in
   Lwt.return_ok ()
