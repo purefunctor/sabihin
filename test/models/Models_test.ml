479a44cc3204cafc4206783b8e9fea0972f3c8a4
Justin Garcia <purefunctor@gmail.com>
2024-03-21 11:35:05 +0800

    Use qualification over the entire list

--- test/models/Models_test.ml
+++ test/models/Models_test.ml
@@ -9,25 +9,27 @@ let () =
       ( "User",
         List.map
           (fun f -> f pool)
-          [
-            User_test.initialize;
-            User_test.insert;
-            User_test.insert_existing;
-            User_test.get_by_id;
-            User_test.get_by_username;
-            User_test.get_missing;
-          ] );
+          User_test.
+            [
+              initialize;
+              insert;
+              insert_existing;
+              get_by_id;
+              get_by_username;
+              get_missing;
+            ] );
       ( "PrivateKey",
         List.map
           (fun f -> f pool)
-          [
-            PrivateKeys_test.initialize;
-            PrivateKeys_test.insert;
-            PrivateKeys_test.insert_existing;
-            PrivateKeys_test.get_by_user_id;
-            PrivateKeys_test.get_by_username;
-            PrivateKeys_test.get_missing;
-          ] );
+          PrivateKeys_test.
+            [
+              initialize;
+              insert;
+              insert_existing;
+              get_by_user_id;
+              get_by_username;
+              get_missing;
+            ] );
       ( "PublicKeys",
         List.map
           (fun f -> f pool)
