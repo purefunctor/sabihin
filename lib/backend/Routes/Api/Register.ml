3225e8393c4d2595186c65c82b52a4a60c3da815
Justin Garcia <purefunctor@gmail.com>
2024-03-23 02:58:31 +0800

    Implement initial form-based registration

--- lib/backend/Routes/Api/Register.ml
+++ lib/backend/Routes/Api/Register.ml
@@ -1,1 +1,46 @@
-let handler _ = Dream.empty `OK
+module RegisterFields = struct
+  type t = { username : string; auth_token : string }
+
+  type e =
+    [ `Missing_Field of string
+    | `Empty_Field of string
+    | `Multiple_Values of string ]
+
+  let render : e -> string = function
+    | `Empty_Field field -> Printf.sprintf "Empty field: %s" field
+    | `Missing_Field field -> Printf.sprintf "Missing field: %s" field
+    | `Multiple_Values field -> Printf.sprintf "Multiple values: %s" field
+
+  module StringMap = Map.Make (String)
+
+  let to_map fields =
+    let aux m (k, v) =
+      match StringMap.find_opt k m with
+      | Some vs -> StringMap.add k (v :: vs) m
+      | None -> StringMap.add k [ v ] m
+    in
+    List.fold_left aux StringMap.empty fields
+
+  let parse (fields : (string * string) list) : (t, [> e ]) result =
+    let field_map = to_map fields in
+    let find_field field =
+      match StringMap.find_opt field field_map with
+      | Some [ "" ] -> Error (`Empty_Field field)
+      | Some [ value ] -> Ok value
+      | Some _ -> Error (`Multiple_Values field)
+      | _ -> assert false
+    in
+    let ( let* ) = Result.bind in
+    let* username = find_field "username" in
+    let* auth_token = find_field "auth_token" in
+    Ok { username; auth_token }
+end
+
+let handler request =
+  let open RegisterFields in
+  match%lwt Dream.form ~csrf:false request with
+  | `Ok fields -> (
+      match parse fields with
+      | Ok _ -> Dream.respond @@ "It's done."
+      | Error error -> Dream.respond ~status:`Bad_Request @@ render error)
+  | _ -> Dream.respond ~status:`Bad_Request "Invalid request."
--- test/backend/Routes_Api_test.ml
+++ test/backend/Routes_Api_test.ml
@@ -2,11 +2,28 @@ open Cohttp
 open Cohttp_lwt_unix
 open Utils
 
+let double_hmac data =
+  let open Mirage_crypto.Hash in
+  let key = Cstruct.of_string "an_obviously_fake_key" in
+  data |> Cstruct.of_string |> SHA512.hmac ~key |> SHA512.hmac ~key
+  |> Cstruct.to_hex_string
+
 let it_works =
   let inner () =
-    let%lwt headers = get_cookie_headers () in
+    let%lwt cookie_headers = get_cookie_headers () in
     let%lwt response, _ =
-      Client.post ~headers (Uri.of_string "http://localhost:8080/api/register")
+      let headers =
+        Header.add cookie_headers "Content-Type"
+          "application/x-www-form-urlencoded"
+      in
+      let body =
+        let username = "purefunctor" in
+        let auth_token = double_hmac "auth_token" in
+        Cohttp_lwt.Body.of_form
+          [ ("username", [ username ]); ("auth_token", [ auth_token ]) ]
+      in
+      Client.post ~headers ~body
+        (Uri.of_string "http://localhost:8080/api/register")
     in
     let code = Response.status response |> Code.code_of_status in
     let _ = Alcotest.(check int) "status code is 200" code 200 in
