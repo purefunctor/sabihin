4619daf22e8b948f24728863f90d4b6c1ee72a16
Justin Garcia <purefunctor@gmail.com>
2024-03-22 21:40:34 +0800

    Add comment about initialize_log

--- test/backend/Backend_test.ml
+++ test/backend/Backend_test.ml
@@ -1,4 +1,8 @@
 let () =
+  (* We need to initialize Dream's log here since we're using Dream.serve
+     to spin up the server for each test case. Alcotest also doesn't seem
+     to use colors if this isn't called, so it's better overall to keep
+     it here. *)
   Dream.initialize_log ();
   Lwt_main.run
   @@ Alcotest_lwt.run "Backend"
