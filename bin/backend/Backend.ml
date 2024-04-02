6edf3c67d98dec70732e3838581dc453b6761f0c
purefunctor <purefunctor@pm.me>
2024-04-02 23:17:49 +0800

    Parse configuration from .env file

--- bin/backend/Backend.ml
+++ bin/backend/Backend.ml
@@ -1,20 +1,42 @@
 let get_database_url () =
   let options = Toolbox.Database.get_options_from_env () in
-  Backend_lib.Logging.info (fun log -> log "SABIHIN_DB_NAME: sabihin");
-  Toolbox.Database.make_url ~options "sabihin"
+  let name =
+    match Sys.getenv_opt "SABIHIN_DB_NAME" with
+    | Some name -> name
+    | None -> "sabihin"
+  in
+  Backend_lib.Logging.info (fun log -> log "SABIHIN_DB_NAME: %s" name);
+  Toolbox.Database.make_url ~options name
 
 let get_server_secret () =
-  match Sys.getenv_opt "SERVER_SECRET" with
+  match Sys.getenv_opt "SABIHIN_SERVER_SECRET" with
   | Some server_secret ->
-      Backend_lib.Logging.info (fun log -> log "SERVER_SECRET: Production Mode");
+      Backend_lib.Logging.info (fun log ->
+          log "SABIHIN_SERVER_SECRET: Production Mode");
       server_secret
   | None ->
       Backend_lib.Logging.warn (fun log ->
-          log "SERVER_SECRET: Development Mode");
+          log "SABIHIN_SERVER_SECRET: Development Mode");
+      Dream.to_base64url (Dream.random 128)
+
+let get_cipher_secret () =
+  match Sys.getenv_opt "SABIHIN_CIPHER_SECRET" with
+  | Some cipher_secret ->
+      Backend_lib.Logging.info (fun log ->
+          log "SABIHIN_CIPHER_SECRET: Production Mode");
+      cipher_secret
+  | None ->
+      Backend_lib.Logging.warn (fun log ->
+          log "SABIHIN_CIPHER_SECRET: Development Mode");
       Dream.to_base64url (Dream.random 128)
 
 let () =
+  Dotenv.export ();
   Dream.initialize_log ();
+
   let database_url = get_database_url () in
   let server_secret = get_server_secret () in
+  let cipher_secret = get_cipher_secret () in
+
+  Backend_lib.Cipher.set_current_key cipher_secret;
   Backend_lib.Server.run ~database_url ~server_secret ()
