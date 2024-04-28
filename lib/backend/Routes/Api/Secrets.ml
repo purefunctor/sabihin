55349a05bbb1980d62a33551d0a566f68148d8d5
Justin Garcia <purefunctor@gmail.com>
2024-04-28 15:52:22 +0800

    internal: add log messages to secrets api

--- lib/backend/Routes/Api/Secrets.ml
+++ lib/backend/Routes/Api/Secrets.ml
@@ -23,22 +23,27 @@ let insert request (user_id : int32) (keys : register_keys_payload) =
 
   Dream.sql request (fun db ->
       let (module Db) = db in
-      Db.with_transaction (fun () ->
-          let* _ =
-            Models.Secrets.insert ~user_id ~client_random_value
-              ~encrypted_master_key ~master_key_iv ~encrypted_protection_key
-              ~protection_key_iv ~exported_verification_key
-              ~encrypted_verification_key ~verification_key_iv
-              ~exported_protection_key db
-          in
-          Lwt.return_ok ()))
+      Models.Secrets.insert ~user_id ~client_random_value ~encrypted_master_key
+        ~master_key_iv ~encrypted_protection_key ~protection_key_iv
+        ~exported_verification_key ~encrypted_verification_key
+        ~verification_key_iv ~exported_protection_key db)
 
 let handler request =
   let inner (session : Session.t) =
     let inner (payload : register_keys_payload) =
       match%lwt insert request session.id payload with
-      | Ok () -> Dream.empty `No_Content
-      | Error _ -> Dream.respond ~status:`Bad_Request "Invalid payload."
+      | Ok () ->
+          Dream.info (fun log -> log "Created Secrets: %s" session.public_id);
+          Dream.empty `No_Content
+      | Error e ->
+          Dream.error (fun log ->
+              let message =
+                match e with
+                | `Msg msg -> msg
+                | #Caqti_error.t as caqti_error -> Caqti_error.show caqti_error
+              in
+              log "%s: failed with %s" session.public_id message);
+          Dream.respond ~status:`Bad_Request "Invalid payload."
     in
     with_json_body request register_keys_payload_of_string inner
   in
